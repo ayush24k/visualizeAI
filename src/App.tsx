88ce60d67eb9f31a4037112b38e08686b6c43f0f
ayush24k <ayushanshu69@gmail.com>
2024-07-09 11:18:09 +0530

    frontend final

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ function App() {
 
   return (
     <div>
+      <p className="text-center p-2 text-[12px]">
+        <a href="https://github.com/ayush24k" target="_blank">@ayush24k</a>
+      </p>
       <AppPage />
     </div>
   )
--- src/components/AppPage.tsx
+++ src/components/AppPage.tsx
@@ -1,14 +1,17 @@
-import { useState } from "react"
+'use client'
+
+import {useState } from "react"
 
 export default function AppPage() {
     const [prompt, setPrompt] = useState("")
     const [image, setImage] = useState("")
+
     return (
-        <div className="flex flex-col justify-center items-center h-screen gap-5">
-            <h1 className="text-[35px] font-semibold text-white">Text-To-Image</h1>
-            <div>
-                <p>Enter Prompt</p>
-                <form className="flex flex-col gap-3">
+        <div className="flex flex-col justify-center items-center gap-5 mt-[90px]">
+            <h1 className="text-[35px] font-semibold text-white">Visualize AI</h1>
+            <p className="mt-[-18px] text-gray-500">convert text to image in a single click</p>
+
+            <div className="p-5 max-w-[650px]">
                     <input
                         className="bg-slate-200 text-gray-800 mt-2 rounded-sm p-1 w-full"
                         type="text"
@@ -16,27 +19,31 @@ export default function AppPage() {
                         value={prompt}
                         onChange={(e) => setPrompt(e.target.value)}
                     ></input>
-                    <button
-                        className="bg-blue-400 p-1 text-black rounded-sm w-full"
-                        onClick={async () => {
-                            const res = await fetch("")
-                            const blob = await res.blob();
-                            setImage(URL.createObjectURL(blob))
-                        }}
-                    >Generate</button>
-                </form>
+
+                <button
+                    className="bg-blue-400 p-1 text-black rounded-sm w-full mt-4"
+                    onClick={async () => {
+                        const res = await fetch(`https://backend-worker.cloud24k.workers.dev/?prompt=${prompt}`)
+                        const blob = await res.blob();
+                        setImage(URL.createObjectURL(blob));
+                    }}
+                >Generate</button>
+                <p className="mt-2 text-[14px] text-gray-500">click on generate and wait for like 3 secs... <br/>built in 49 minutes, might not work..</p>
             </div>
 
             <div className="mt-10 flex flex-col justify-center items-center">
                 <p>Genrated Image</p>
                 <div className="p-4">
                     <img
-                        className="rounded-md shadow-sm"
-                        src={image ? image : "./placeholder.jpg"}
+                        alt="generated img"
+                        className="rounded-md shadow-xl border-2 border-purple-900 p-2"
+                        height={400}
+                        src={image ? image : "./placeholder.png"}
                         style={{
                             aspectRatio: 600 / 400,
                             objectFit: "cover"
                         }}
+                        width={600}
                     />
                 </div>
             </div>
